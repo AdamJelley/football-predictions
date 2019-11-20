204aa2956ffcd0e5b6093e207dcb4665baa4bac6
ajelley <ajelley>
2019-11-20 17:21:58 +0100

    Saved recipe: FOOTBALLMATCHPREDICTIONS.compute_Fixtures

--- recipes/compute_Fixtures.py
+++ recipes/compute_Fixtures.py
@@ -1,17 +1,41 @@
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
+import requests
 
 leagues_df = dataiku.Dataset("Leagues_prepared_filtered").get_dataframe()
 
-# Compute recipe outputs
-# TODO: Write here your actual code that computes the outputs
-# NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
+base_url = 'https://api-football-v1.p.rapidapi.com/v2/'
 
-fixtures_df = ... # Compute a Pandas dataframe to write into Fixtures
+querystring = {"timezone":"Europe/London"}
 
+headers = {
+    'x-rapidapi-host': "api-football-v1.p.rapidapi.com",
+    'x-rapidapi-key': "6feb679189msh0e2593854adc7e5p1c15a8jsnbaa3c48b1a11"
+    }
+
+def getRemainingRequests(base_url, headers, querystring):
+    status_url = base_url + '/seasons'
+    response = requests.request("GET", status_url, headers=headers, params=querystring)
+    remaining = int(response.headers['X-RateLimit-requests-Remaining'])
+    return remaining
+
+def getFixturesByLeague(league_id, base_url, headers, querystring):
+    """Get fixtures dataframe for particular league id"""
+    if getRemainingRequests(base_url, headers, querystring) > 0:
+        fixtures_url = base_url + '/fixtures/league/' + str(league_id)
+        fixtures_response = requests.request("GET", fixtures_url, headers=headers, params=querystring)
+        fixtures_df = pd.DataFrame(fixtures_response.json()['api']['fixtures'])
+    else:
+        raise ValueError('No remaining requests. Try again tomorrow.')
+    return fixtures_df
+
+fixtures_df = pd.DataFrame()
+for league_id in leagues_df['league_id']:
+    df = getFixturesByLeague(league_id, base_url, headers, querystring)
+    print(str(league_id) + ' done: ' + str(df.shape))
+    fixtures_df = fixtures_df.append(df)
 
 # Write recipe outputs
 fixtures = dataiku.Dataset("Fixtures")
