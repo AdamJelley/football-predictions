7d7636f3866b707ad4b9345664df35c94da7472c
ajelley <ajelley>
2019-11-22 19:27:10 +0100

    Saved recipe: FOOTBALLMATCHPREDICTIONS.compute_Upcoming_Fixtures

--- recipes/compute_Upcoming_Fixtures.py
+++ recipes/compute_Upcoming_Fixtures.py
@@ -16,12 +16,12 @@ startDate = pd.to_datetime('today')
 endDate = (pd.to_datetime('today') + pd.Timedelta('6 days'))
 upcomingDates = list(pd.date_range(startDate, endDate).strftime('%Y-%m-%d'))
 
-upcoming_fixtures_df = pd.DataFrame()
+upcoming_Fixtures_df = pd.DataFrame()
 for date in upcomingDates:
     df = API_Functions.getFixturesByDate(date, BASE_URL, headers, QUERYSTRING)
     print(str(date) + ' done: ' + str(df.shape))
-    upcoming_fixtures_df = upcoming_fixtures_df.append(df)
-    upcoming_fixtures_df = upcoming_fixtures_df[upcoming_fixtures_df['league_id'].isin(leagues_prepared_filtered_df['league_id'])]
+    upcoming_Fixtures_df = upcoming_Fixtures_df.append(df)
+    upcoming_Fixtures_df = upcoming_Fixtures_df[upcoming_Fixtures_df['league_id'].isin(leagues_prepared_filtered_df['league_id'])]
 
 # Write recipe outputs
 upcoming_Fixtures = dataiku.Dataset("Upcoming_Fixtures")
