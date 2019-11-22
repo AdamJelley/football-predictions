2304a4e9432c0b25089b4517c2d0b068bbda4167
ajelley <ajelley>
2019-11-22 10:19:45 +0100

    Saved recipe: FOOTBALLMATCHPREDICTIONS.compute_Fixtures

--- recipes/compute_Fixtures.py
+++ recipes/compute_Fixtures.py
@@ -7,6 +7,31 @@ import API_Functions
 
 leagues_df = dataiku.Dataset("Leagues_prepared_filtered").get_dataframe()
 
+import dataiku
+
+BASE_URL = 'https://api-football-v1.p.rapidapi.com/v2/'
+
+QUERYSTRING = {"timezone":"Europe/London"}
+
+def getAPIRequestHeaders():
+    client = dataiku.api_client()
+    auth_info = client.get_auth_info(with_secrets=True)
+
+    x-rapidapi-key = None
+    for secret in auth_info["secrets"]:
+        if secret["key"] == "x-rapidapi-key":
+            x-rapidapi-key = secret["value"]
+            break
+    if not x-rapidapi-key:
+            raise Exception("x-rapidapi-key not found")
+
+    headers = {
+        'x-rapidapi-host': "api-football-v1.p.rapidapi.com",
+        'x-rapidapi-key': x-rapidapi-key
+        }
+        
+    return headers
+
 headers = API_Functions.getAPIRequestHeaders()
 
 fixtures_df = pd.DataFrame()
