6716b083774b01f10651e9c39a553e153e98eea7
ajelley <ajelley>
2019-11-22 10:23:43 +0100

    Saved recipe: FOOTBALLMATCHPREDICTIONS.compute_Fixtures

--- recipes/compute_Fixtures.py
+++ recipes/compute_Fixtures.py
@@ -17,17 +17,17 @@ def getAPIRequestHeaders():
     client = dataiku.api_client()
     auth_info = client.get_auth_info(with_secrets=True)
 
-    x-rapidapi-key = None
+    x_rapidapi_key = None
     for secret in auth_info["secrets"]:
         if secret["key"] == "x-rapidapi-key":
-            x-rapidapi-key = secret["value"]
+            x_rapidapi_key = secret["value"]
             break
-    if not x-rapidapi-key:
+    if not x_rapidapi_key:
             raise Exception("x-rapidapi-key not found")
 
     headers = {
         'x-rapidapi-host': "api-football-v1.p.rapidapi.com",
-        'x-rapidapi-key': x-rapidapi-key
+        'x-rapidapi-key': x_rapidapi_key
         }
         
     return headers
