4e9f502b3da6567d0af3bd2cb1e0687a16415b37
ajelley <ajelley>
2019-12-02 13:25:09 +0100

    Saved web app FootballPredictions (FOOTBALLMATCHPREDICTIONS.JidtqGs)

--- web_apps/JidtqGs/backend.py
+++ web_apps/JidtqGs/backend.py
@@ -24,7 +24,7 @@ def get_predictions():
 def get_history():
     max_rows = request.args.get('max_rows') if 'max_rows' in request.args else 100
     mydataset = dataiku.Dataset("Historical_Fixtures_Evaluated")
-    df = mydataset.get_dataframe(sampling='head', limit=max_rows)
+    df = mydataset.get_dataframe(sampling='tail', limit=max_rows)
     df = df[['event_date', 'homeTeam_team_name', 'awayTeam_team_name', 'league_name', 'venue', 'proba_Home', 'proba_Away', 'proba_Draw', 'prediction', 'target', 'prediction_correct']]
     df = df.sort_values('event_date', ascending=False)
     data = df.to_html(index=False)
