179d2ddc4ef54f2a3cb87794ec68ad86270b4884
ajelley <ajelley>
2020-01-03 16:39:09 +0100

    edit files

--- lib/python/API_Functions.py
+++ lib/python/API_Functions.py
@@ -25,28 +25,33 @@ def getAPIRequestHeaders():
         
     return headers
 
-def getRemainingRequests(base_url, headers, querystring):
+def testRemainingRequests(base_url, headers, querystring):
+    """Test remaining requests: fail if no requests remaining this month and give warning if < 100."""
     status_url = base_url + '/seasons'
     response = requests.request("GET", status_url, headers=headers, params=querystring)
     remaining = int(response.headers['X-RateLimit-requests-Remaining'])
-    return remaining
+    if remaining <= 0:
+        raise ValueError('No remaining requests. Try again tomorrow.')
+    elif remaining < 100:
+        print('Warning: Only ' + str(remaining) + ' Requests Remaining')
+    return 1
 
 def getFixturesByLeague(league_id, base_url, headers, querystring):
     """Get fixtures dataframe for particular league id"""
-    if getRemainingRequests(base_url, headers, querystring) > 0:
-        fixtures_url = base_url + '/fixtures/league/' + str(league_id)
-        fixtures_response = requests.request("GET", fixtures_url, headers=headers, params=querystring)
-        fixtures_df = pd.DataFrame(fixtures_response.json()['api']['fixtures'])
-    else:
-        raise ValueError('No remaining requests. Try again tomorrow.')
+    testRemainingRequests(base_url, headers, querystring)
+    
+    fixtures_url = base_url + '/fixtures/league/' + str(league_id)
+    fixtures_response = requests.request("GET", fixtures_url, headers=headers, params=querystring)
+    fixtures_df = pd.DataFrame(fixtures_response.json()['api']['fixtures'])
+
     return fixtures_df
 
 def getFixturesByDate(date, base_url, headers, querystring):
     """Get fixtures dataframe for particular league id for a particular date"""
-    if getRemainingRequests(base_url, headers, querystring) > 0:
-        date_fixtures_url = base_url + '/fixtures/date/' + str(date)
-        date_fixtures_response = requests.request("GET", date_fixtures_url, headers=headers, params=querystring)
-        date_fixtures_df = pd.DataFrame(date_fixtures_response.json()['api']['fixtures'])
-    else:
-        raise ValueError('No remaining requests. Try again tomorrow.')
+    testRemainingRequests(base_url, headers, querystring)
+    
+    date_fixtures_url = base_url + '/fixtures/date/' + str(date)
+    date_fixtures_response = requests.request("GET", date_fixtures_url, headers=headers, params=querystring)
+    date_fixtures_df = pd.DataFrame(date_fixtures_response.json()['api']['fixtures'])
+
     return date_fixtures_df
