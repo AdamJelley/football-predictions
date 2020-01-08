c313a3ab2e3bffb4f1994a2bff6818d63329468a
ajelley <ajelley>
2020-01-08 18:38:46 +0100

    Saved recipe: FOOTBALLMATCHPREDICTIONS.compute_Latest_Player_Elo_Ranks

--- recipes/compute_Latest_Player_Elo_Ranks.sql
+++ recipes/compute_Latest_Player_Elo_Ranks.sql
@@ -1,4 +1,4 @@
-SELECT d."playerId"::INTEGER, d."playerName", d."Elo_rank"
+SELECT d."playerId"::INTEGER, d."playerName", d."playerTeam", d."Elo_rank"
 FROM 
     (
     SELECT c.*, ROW_NUMBER()
@@ -6,14 +6,14 @@ FROM
             ORDER BY "event_date" desc)
     FROM
         (
-            SELECT a."event_date" as "event_date", a."player_id_home" as "playerId", a."player_home" as "playerName", a."player_id_home_new_rank" as "Elo_rank" FROM
+            SELECT a."event_date" as "event_date", a."player_id_home" as "playerId", a."player_home" as "playerName", a."homeTeam_team_name" as "playerTeam", a."player_id_home_new_rank" as "Elo_rank" FROM
               (SELECT *, ROW_NUMBER()
               OVER (PARTITION BY "player_id_home"
               ORDER BY "event_date" desc)
               FROM "FOOTBALLMATCHPREDICTIONS_player_elo_ranks_prepared") a
               WHERE a.ROW_NUMBER = 1
         UNION
-            SELECT b."event_date" as "event_date", b."player_id_away" as "playerId", b."player_away" as "playerName", b."player_id_away_new_rank" as "Elo_rank" FROM
+            SELECT b."event_date" as "event_date", b."player_id_away" as "playerId", b."player_away" as "playerName", b."awayTeam_team_name" as "playerTeam", b."player_id_away_new_rank" as "Elo_rank" FROM
               (SELECT *, ROW_NUMBER()
               OVER (PARTITION BY "player_id_away"
               ORDER BY "event_date" desc)
