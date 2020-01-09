776b9bd53066e341c03ec28fdc69a1bfac280d51
ajelley <ajelley>
2020-01-09 12:10:45 +0100

    Saved recipe: FOOTBALLMATCHPREDICTIONS.compute_Latest_Team_Elo_Ranks_1

--- recipes/compute_Latest_Team_Elo_Ranks_1.sql
+++ recipes/compute_Latest_Team_Elo_Ranks_1.sql
@@ -14,7 +14,7 @@ FROM
                 a."player_id_home_new_rank_min" as "player_rank_min",
                 a."player_id_home_new_rank_max" as "player_rank_max",
                 a."player_id_home_new_rank_avg" as "player_rank_avg",
-                a."player_id_home_new_rank_stddev" as "player_rank_stddev",
+                a."player_id_home_new_rank_stddev" as "player_rank_stddev"
             FROM
               (SELECT *, ROW_NUMBER()
               OVER (PARTITION BY "homeTeam_team_id"
@@ -30,7 +30,7 @@ FROM
                 b."player_id_away_new_rank_min" as "player_rank_min",
                 b."player_id_away_new_rank_max" as "player_rank_max",
                 b."player_id_away_new_rank_avg" as "player_rank_avg",
-                b."player_id_away_new_rank_stddev" as "player_rank_stddev",
+                b."player_id_away_new_rank_stddev" as "player_rank_stddev"
             FROM
               (SELECT *, ROW_NUMBER()
               OVER (PARTITION BY "awayTeam_team_id"
