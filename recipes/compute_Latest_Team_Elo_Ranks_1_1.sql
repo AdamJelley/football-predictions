7fc74d96e8decd7129e697dad91aaea5d2deca6a
ajelley <ajelley>
2020-01-24 18:33:16 +0100

    Saved recipe: FOOTBALLMATCHPREDICTIONS.compute_Latest_Team_Elo_Ranks_1_1

--- recipes/compute_Latest_Team_Elo_Ranks_1_1.sql
+++ recipes/compute_Latest_Team_Elo_Ranks_1_1.sql
@@ -1,4 +1,15 @@
-SELECT d."teamId"::INTEGER, d."teamName", d."Elo_rank", d."player_rank_min", d."player_rank_max", d."player_rank_avg", d."player_rank_stddev", d."player_id_concat"
+SELECT d."teamId"::INTEGER, d."teamName", d."Elo_rank", d."player_rank_min", d."player_rank_max", d."player_rank_avg", d."player_rank_stddev", 
+d."player_0",
+d."player_1",
+d."player_2",
+d."player_3",
+d."player_4",
+d."player_5",
+d."player_6",
+d."player_7",
+d."player_8",
+d."player_9",
+d."player_10"
 FROM 
     (
     SELECT c.*, ROW_NUMBER()
@@ -15,7 +26,6 @@ FROM
                 a."player_id_home_new_rank_max" as "player_rank_max",
                 a."player_id_home_new_rank_avg" as "player_rank_avg",
                 a."player_id_home_new_rank_stddev" as "player_rank_stddev",
-                a."player_id_home_concat" as "player_id_concat",
                 a."home_player_0" as "player_0",
                 a."home_player_1" as "player_1",
                 a."home_player_2" as "player_2",
@@ -43,18 +53,17 @@ FROM
                 b."player_id_away_new_rank_max" as "player_rank_max",
                 b."player_id_away_new_rank_avg" as "player_rank_avg",
                 b."player_id_away_new_rank_stddev" as "player_rank_stddev",
-                b."player_id_away_concat" as "player_id_concat",
-                a."away_player_0" as "player_0",
-                a."away_player_1" as "player_1",
-                a."away_player_2" as "player_2",
-                a."away_player_3" as "player_3",
-                a."away_player_4" as "player_4",
-                a."away_player_5" as "player_5",
-                a."away_player_6" as "player_6",
-                a."away_player_7" as "player_7",
-                a."away_player_8" as "player_8",
-                a."away_player_9" as "player_9",
-                a."away_player_10" as "player_10"
+                b."away_player_0" as "player_0",
+                b."away_player_1" as "player_1",
+                b."away_player_2" as "player_2",
+                b."away_player_3" as "player_3",
+                b."away_player_4" as "player_4",
+                b."away_player_5" as "player_5",
+                b."away_player_6" as "player_6",
+                b."away_player_7" as "player_7",
+                b."away_player_8" as "player_8",
+                b."away_player_9" as "player_9",
+                b."away_player_10" as "player_10"
             FROM
               (SELECT *, ROW_NUMBER()
               OVER (PARTITION BY "awayTeam_team_id"
